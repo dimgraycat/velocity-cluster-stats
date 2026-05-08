d149acdd6bf2e577b41632d198077cb6ae2a1367
dimgraycat <dimgray.cat@gmail.com>
2026-05-09 07:26:25 +0900

    bump v0.2.0

--- src/main/java/net/dmgct/velocityclusterstats/BuildConstants.java
+++ src/main/java/net/dmgct/velocityclusterstats/BuildConstants.java
@@ -7,7 +7,7 @@ public final class BuildConstants {
     /**
      * Plugin version used for Gradle artifacts and {@code velocity-plugin.json}.
      */
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "0.2.0";
 
     private BuildConstants() {
     }
